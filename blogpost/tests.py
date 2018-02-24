7c69028a7d12873405814e357aa52006a531bf32
lynn8570 <lynn8570@gmail.com>
2018-02-24 09:06:59 +0800

    test case for path without create data

--- blogpost/tests.py
+++ blogpost/tests.py
@@ -1,3 +1,18 @@
+from django.urls import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 
 # Create your tests here.
+
+from blogpost.views import index, view_post
+
+class HomePageTest(TestCase):
+	"""docstring for HomePageTest"""
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, index)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = index(request)
+		self.assertIn(b'<title>Welcome to my blog</title>',response.content)
